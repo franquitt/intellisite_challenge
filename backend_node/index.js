b23bad299c124326ecd9724547d3060a819d603c
Franco Agresta Gonzalez <franquitt@gmail.com>
2021-01-30 17:31:05 -0300

    start swagger docs

--- backend_node/index.js
+++ backend_node/index.js
@@ -1,10 +1,49 @@
 const express = require("express");
+const bodyParser = require("body-parser"),
+	swaggerJsdoc = require("swagger-jsdoc"),
+	swaggerUi = require("swagger-ui-express");
 let app = express();
-const bodyParser = require("body-parser");
 
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Intellisite challenge",
+			version: "0.1.0",
+			description:
+				"This is a project that solves the challenge of intellisite for the fullstack position",
+			license: {
+				name: "MIT",
+				url: "https://spdx.org/licenses/MIT.html",
+			},
+			contact: {
+				name: "Franco Agresta González",
+				url: "https://github.com/franquitt/intellisite_challenge",
+				email: "franquitt@gmail.com",
+			},
+		},
+		servers: [
+			{
+				url: "http://localhost:8000/api",
+			},
+		],
+		tags: [{
+			name: "anomalies",
+			description: "Everything about the traffic anomalies"
+		}]
+
+	},
+	apis: ["./routes/anomaly_routes.js"],
+};
+const specs = swaggerJsdoc(options);
+app.use(
+	"/api/docs",
+	swaggerUi.serve,
+	swaggerUi.setup(specs)
+);
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 //app.use("/", router);
 app.use("/api/anomalies", require("./routes/anomaly_routes"));
--- backend_node/routes/anomaly_routes.js
+++ backend_node/routes/anomaly_routes.js
@@ -2,7 +2,47 @@ const express = require("express");
 const router = express.Router();
 const anomalyController = require("../controller/anomaly_controller");
 
+/**
+ * @swagger
+ *
+ * /anomalies/:
+ *   get:
+ *     tags:
+ *       - "anomalies"
+ *     summary: Returns a list of all the generated anomalies.
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: OK
+ *         schema:
+ *           type: object
+ *           properties:
+ *             module:
+ *               type: string
+ *             module_id:
+ *               type: string
+ *
+ */
 router.get("/", anomalyController.findAll);
+
+/**
+ * @swagger
+ *
+ * /anomalies/csvfile:
+ *   get:
+ *     tags:
+ *       - "anomalies"
+ *     summary: Returns a CSV file with stats, grouped by date and time.
+ *     produces:
+ *       - text/csv
+ *     responses:
+ *       200:
+ *         description: OK
+ *         schema:
+ *           type: file
+ *
+ */
 router.get("/csvfile", anomalyController.csvfile);
 
 module.exports = router;
